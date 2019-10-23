b4cc47cde0c6b6889cb9b2c5cb1296aaa5d6b147
ali.official001 <ali.offical001@gmail.com>
2019-10-23 17:47:01 +0500

    Adding assignment 22

---
+++ 22jsAssign/app.js
@@ -0,0 +1,37 @@
+//Q1
+// var employeeStatus = {
+//     id : 123,
+//     name : "Ali",
+//     general : true,
+//     staus : function(presentDays){
+//         if(presentDays >= 28){
+//             console.log(this.name + ' is punctual');
+//         } else if(presentDays < 28) {
+//             console.log(this.name + " need to improve puntuality");
+//         }else {
+//             console.log("Please enter number of present days");
+//         }
+//     } 
+// }
+
+// // console.log(employeeStatus.staus(28));
+
+
+//Q2
+// var CreateEmploy = function(name,SName,id,department){
+//     this.Name = name,
+//     this.Second_Name = SName,
+//     this.Id = id,
+//     this.Department = department
+// }
+
+// var employ1 = new CreateEmploy('Ali','Ansari',01,'Supply chain');
+// var employ2 = new CreateEmploy('Iqbal','khan', 02, 'Admin');
+
+// console.log(employ2);
+
+
+//Q3
+// CreateEmploy.prototype.firm = "Gul Ahmed";
+// employ2.firm = "Al-Karam"
+// console.log(employ2.firm);
